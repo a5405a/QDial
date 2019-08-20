3faf874b53657b0d741694348a518aa9c4bf73a2
a5405a <a5405a@yahoo.com.tw>
2019-08-20 17:47:59 +0800

    Add files via upload

---
+++ QDial_project_190819_v3/QDial/main.cpp
@@ -0,0 +1,24 @@
+#include "widget.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Widget w;
+
+    w.setStyleSheet("background-color:white;");
+
+    w.show();
+
+//    https://blog.csdn.net/chenlong12580/article/details/7428733
+    QStringList keyList = QStyleFactory::keys();
+
+    for(int i = 0; i < keyList.length(); i++)
+    {
+        qDebug(keyList.at(i).toUtf8());
+    }
+
+
+
+    return a.exec();
+}
